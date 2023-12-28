280d9380fa1d08fcb48130815d85ea29c7be71b0
amrendra03 <amrendra9076893685@gmail.com>
2023-12-28 21:12:29 +0530

    Employee-api-development -completed

--- API_Development_using_Python/companyapi/api/views.py
+++ API_Development_using_Python/companyapi/api/views.py
@@ -4,12 +4,29 @@ from rest_framework import viewsets
 
 from api.models import Company
 from api.serializers import CompanySerializer, EmployeeSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+    # companies/{companies id}/employees
+    @action(detail=True, methods=['get'])
+    def employees(self, request, pk=None):
+        print("get employees of ", pk, 'company')
+        try:
+            company = Company.objects.get(pk=pk)
+            emps = Employee.objects.filter(company=company)
+
+            emps_serializer = EmployeeSerializer(emps, many=True, context={'request': request})
+
+            return Response(emps_serializer.data)
+        except Exception as e:
+            print(e)
+            return Response({'message': 'failed to load'})
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
