fb5502b7336f55a526851c64684a6c712eb99841
amrendra03 <amrendra9076893685@gmail.com>
2023-12-28 18:21:02 +0530

    company-api-development -completed

--- API_Development_using_Python/companyapi/companyapi/settings.py
+++ API_Development_using_Python/companyapi/companyapi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'companyapi.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "companyapi",
+        "USER": "root",
+        "PASSWORD": "system890",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
