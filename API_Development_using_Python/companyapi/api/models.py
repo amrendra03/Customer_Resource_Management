01a908fa5eb164743f45432dcc9d99b2e2ac7450
amrendra03 <amrendra9076893685@gmail.com>
2023-12-28 18:41:20 +0530

    Employee-api-development -init

--- API_Development_using_Python/companyapi/api/models.py
+++ API_Development_using_Python/companyapi/api/models.py
@@ -15,4 +15,19 @@ class Company(models.Model):
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
+
 # Employee Models
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50,
+                                choices=(('Manager', 'manager'), ('Software Developer', 'SDE'), ('Project Leader',
+                                                                                                 'Project Leader')))
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
--- API_Development_using_Python/companyapi/api/views.py
+++ API_Development_using_Python/companyapi/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
-from api.models import Company
+from api.models import Company, Employee
 from rest_framework import viewsets
 
 from api.models import Company
-from api.serializers import CompanySerializer
+from api.serializers import CompanySerializer, EmployeeSerializer
 
 
 # Create your views here.
@@ -11,3 +11,7 @@ class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
